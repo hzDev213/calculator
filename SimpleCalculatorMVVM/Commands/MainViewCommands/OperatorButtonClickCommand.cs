547e5a16631d20f2102348867b5bc47c284f21fc
doubl1k <gregexchangeshdh@gmail.com>
2026-04-02 11:45:35 +0300

    Добавлен класс для команды оператора

---
+++ SimpleCalculatorMVVM/Commands/MainViewCommands/OperatorButtonClickCommand.cs
@@ -0,0 +1,40 @@
+﻿using SimpleCalculatorMVVM.Services.Calculators;
+
+namespace SimpleCalculatorMVVM.Commands.MainViewCommands
+{
+    internal class OperatorButtonClickCommand : Command
+    {
+        CalculatorEngine _calculator;
+
+        Action<string> _setText;
+        Func<string> _getText;
+        Action<string> _setHistory;
+        OperatorButtonClickCommand(Action<string> SetText, Func<string> GetText, Action<string> SetHistory)
+        {
+            _calculator = new CalculatorEngine();
+
+            _getText = GetText;
+            _setText = SetText;
+            _setHistory = SetHistory;
+        }
+        public override bool CanExecute(object? p) => true;
+        public override void Execute(object? p)
+        {
+            string[] operators = { "+", "-", "*", "/" };
+
+            string? operator_ = p?.ToString();
+            if (operator_ != null)
+            {
+                if (!operators.Any(op => _getText().Contains(op)))
+                {
+                    _setText(_getText() == "0" ? "0" : _getText() + operator_);
+                }
+                else
+                {
+                    _setHistory(_getText());
+                    _setText(_calculator.Calculate(_getText()) + operator_);
+                }
+            }
+        }
+    }
+}
