4f1d0b3ca0f53991bcd327a876dfc33e9e69edf6
doubl1k <gregexchangeshdh@gmail.com>
2026-04-05 22:11:57 +0300

    Оптимизированы using'и

--- SimpleCalculatorMVVM/ViewModels/MainViewModel.cs
+++ SimpleCalculatorMVVM/ViewModels/MainViewModel.cs
@@ -1,12 +1,9 @@
 ﻿using SimpleCalculatorMVVM.Commands.MainViewCommands;
-using SimpleCalculatorMVVM.Factories.ButtonFactories;
-using SimpleCalculatorMVVM.Models.Buttons;
 using SimpleCalculatorMVVM.Services.Calculators;
 using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
 
 namespace SimpleCalculatorMVVM.ViewModels
 {
