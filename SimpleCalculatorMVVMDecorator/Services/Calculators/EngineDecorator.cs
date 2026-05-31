e148beacf45a67141220a0f9253a238325237497
doubl1k <gregexchangeshdh@gmail.com>
2026-05-31 21:25:28 +0300

    Добавил базовый декоратор

---
+++ SimpleCalculatorMVVMDecorator/Services/Calculators/EngineDecorator.cs
@@ -0,0 +1,13 @@
+﻿namespace SimpleCalculatorMVVMDecorator.Services.Calculators
+{
+    public class EngineDecorator : ICalculator
+    {
+        private ICalculator _wrappee;
+        EngineDecorator(ICalculator wrappee) => _wrappee = wrappee;
+
+        virtual public string Calculate(string expression)
+        {
+            return _wrappee.Calculate(expression);
+        }
+    }
+}
